f4f9334521df3c1aa98fce0059558e8dca3d4715
Hsuiiii <60276239+Hsuiiii@users.noreply.github.com>
2020-05-01 06:19:46 -0400

    add

---
+++ mainnav.js
@@ -0,0 +1,44 @@
+document.getElementById("navMenu").innerHTML =
+     '<ul>'+
+     '<li><a href="index.html">Home</a></li>'+
+     '<li><a href="cs.html">About Tk+</a></li>'+
+     '<li><a href="buynow.html">Inspiration</a></li>'+
+     '<li><a href="">Contact us</a></li>'+
+     '</ul>';
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
+
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides1");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
